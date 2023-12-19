44f63414212c1f883617ed40f1cfc566f599c7c1
Subrata Das <subrata@riktamtech.com>
2023-12-19 10:29:45 +0530

    new changes made in uimodule

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,6 +1,8 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 
+// New changes made in uimodule
+
 const Button = ({title}) => {
   return (
     <View style={{backgroundColor: 'red', padding: 10, borderRadius: 5}}>
