d4dfe7f346a9294cb8ac31f7eaf7f517ab3edc62
Subrata Das <subrata@riktamtech.com>
2023-12-19 10:26:35 +0530

    changes made in button

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,10 +1,10 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 
-const Button = () => {
+const Button = ({title}) => {
   return (
-    <View style={{backgroundColor: 'red'}}>
-      <Text>Button</Text>
+    <View style={{backgroundColor: 'red', padding: 10, borderRadius: 5}}>
+      <Text style={{color: '#FFFFFF'}}>{title}</Text>
     </View>
   );
 };
